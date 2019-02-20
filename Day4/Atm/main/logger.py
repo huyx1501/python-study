598a30d0d027473628b3e0e0eefb0177c72f3b8b
huyx1501 <huyx1501@gmail.com>
2019-02-20 23:19:46 +0800

    日志模块增加注释

--- Day4/Atm/main/logger.py
+++ Day4/Atm/main/logger.py
@@ -1,11 +1,19 @@
 from conf import config
 import time
 
-date = time.strftime("%Y-%m-%d", time.localtime())
-datetime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+date = time.strftime("%Y-%m-%d", time.localtime())  # 获取当前的年月日，用于日志文件名
+datetime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())  # 获取当前的具体时间，用于日志内容
 
 
 def logger(uid="", level="INFO", scene="", msg=""):
+    '''
+    日志记录模块
+    :param uid: 操作用户的ID
+    :param level: 日志级别，默认INFO
+    :param scene: 操作场景，如提现，还款等
+    :param msg: 具体消息内容
+    :return: None
+    '''
     data = "[%s] [%s] [UID=%s] [%s] - %s" % (datetime, level, uid, scene, msg)
     path = "%s\%s%s%s" \
            % (config.log_conf["log_path"], config.log_conf["log_prefix"], date, config.log_conf["log_suffix"])
