40d76de19adf390f8bafba0297cdefeca5b6021f
huyx1501 <huyx1501@gmail.com>
2019-02-28 10:44:31 +0800

    路径配置兼容性

--- Day4/Atm/conf/config.py
+++ Day4/Atm/conf/config.py
@@ -3,11 +3,15 @@ import sys
 
 ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
+db_dir = "db"  # 数据库目录相对根目录的路径
+log_dir = "logs"  # 日志目录相对根目录的路径
+
+
 # 数据库配置
 database = {
     "db_type": "file",  # 数据库类型，可以是file, mysql等，现在只支持file
     "db_name": "accounts",  # 数据库名，如果是file,则表示数据库所在文件夹
-    "db_path": "%s\db" % ROOT,  # 数据库路径, mysql不需要
+    "db_path": os.path.join(ROOT, db_dir),  # 请勿修改，请修改db_dir配置
     "db_user": "",  # 数据库用户名
     "db_pass": ""  # 数据密码
 }
@@ -21,7 +25,7 @@ transaction = {
 
 # 日志相关配置
 log_conf = {
-    "log_path": "%s\logs" % ROOT,  # 日志文件保存目录
+    "log_path": os.path.join(ROOT, log_dir),  # 请勿修改，请修改log_dir配置
     "log_prefix": "ATMLog-",  # 日志文件前缀
-    "log_suffix": ".log"  #日志文件后缀
+    "log_suffix": ".log"  # 日志文件后缀
 }
