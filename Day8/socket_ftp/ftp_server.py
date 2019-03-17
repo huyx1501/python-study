7b5cbfc5e2a02e1e8b859f8660522ffbb27a869b
huyx1501 <huyx1501@gmail.com>
2019-03-17 11:05:38 +0800

    客户端断开连接时的处理

--- Day8/socket_ftp/ftp_server.py
+++ Day8/socket_ftp/ftp_server.py
@@ -15,8 +15,16 @@ while True:
     print("与客户端%s在%s端口连接成功" % (addr[0], addr[1]))
     m = hashlib.md5()  # 创建MD5对象
     while True:
-        # 接收客户端指令并进行分割
-        cmd = conn.recv(1024).decode().split()
+        try:
+            # 接收客户端指令并进行分割
+            receive_data = conn.recv(1024).decode()
+            if not receive_data: # 当客户端断开时，Linux下会收到空数据包
+                print("客户端已断开连接")
+                break
+        except ConnectionResetError:  # 当客户端断开时，Windows下会抛出异常
+            print("客户端已断开连接")
+            break
+        cmd = receive_data.split()
         method, file_path = cmd[0], cmd[1]
         if os.path.isfile(file_path) and method == "get":  # 判断文件是否存在
             file_size = os.stat(file_path).st_size  # 获取文件大小信息
--- Day8/socket_ssh/ssh_server.py
+++ Day8/socket_ssh/ssh_server.py
@@ -14,17 +14,23 @@ while True:
     print("成功建立连接...", client_addr)
     while True:
         # 接受数据存入变量
-        data = conn.recv(1024)
-        if not data:
+        try:
+            data = conn.recv(1024)
+            if not data:  # 当客户端断开时，Linux下会收到空数据包
+                print("Client has gone away...")
+                break
+        except ConnectionResetError:  # 当客户端断开时，Windows下会抛出异常
             print("Client has gone away...")
             break
-        else:
-            cmd = data.decode("utf-8")  # 解码接收的指令
-            cmd_result = os.popen(cmd).read()  # 执行指令
-            print("执行指令 %s" % cmd)
+        cmd = data.decode("utf-8")  # 解码接收的指令
+        cmd_result = os.popen(cmd).read()  # 执行指令
+        print("执行指令 %s" % cmd)
+        if cmd_result:
             result_size = len(cmd_result.encode("utf-8"))
             conn.send(str(result_size).encode("utf-8"))  # 先发送结果长度
             print("结果长度：%s" % result_size)
-            conn.recv(1024)  # 等待客户端回应
+            conn.recv(1024)  # 等待客户端回应，避免粘包
             conn.send(cmd_result.encode("utf-8"))  # 发送命令执行结果
-            print("发送完成")
+        else:
+            conn.send("指令错误".encode())
+        print("发送完成")
