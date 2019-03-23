f08a162dbb5ecb08278cd61bdf52db6d7ddef47e
huyx1501 <huyx1501@gmail.com>
2019-03-23 16:15:46 +0800

    增加日志模块，修复cd命令根目录BUG

--- Day8/MyFTP/server/core/server.py
+++ Day8/MyFTP/server/core/server.py
@@ -8,6 +8,7 @@ import yaml  # 用于解析配置文件
 import hashlib
 import sys
 import shutil
+import time
 
 
 # 定义Handler类
@@ -17,30 +18,33 @@ class TCPHandler(socketserver.BaseRequestHandler):
         用于进行远程用户认证
         :return: 认证通过返回用户信息
         """
+        sep = os.sep
         users = config["users"]
         self.request.send(b"Login")
         login_times = 0
         while login_times < 3:
+            m = hashlib.md5()
+            auth_data = self.request.recv(1024).decode().split()  # 接收认证消息
             try:
-                m = hashlib.md5()
-                auth_data = self.request.recv(1024).decode().split()  # 接收认证消息
                 user_info = users[auth_data[0]]  # 根据用户名查询用户信息
                 if user_info:
                     password = user_info["key"]  # 取出用户密码
                     m.update(password.encode("utf-8"))
                     pass_md5 = m.hexdigest()
                     if pass_md5 == auth_data[1]:
-                        pwd = config["users"][user_info["name"]]["pwd"]
+                        pwd = config["users"][user_info["name"]]["pwd"].rstrip(sep)
                         if not pwd:  # pwd未设置
-                            pwd = user_info["home"]
-                            config["users"][user_info["name"]]["pwd"] = user_info["home"]
+                            pwd = user_info["home"].rstrip(sep)
+                            config["users"][user_info["name"]]["pwd"] = pwd
                         self.request.send(("Success" + " " + pwd).encode("utf-8"))
                         return user_info["name"]
                     else:
+                        logger("用户[%s]登陆失败：密码错误" % auth_data[0], "ERROR", str(self.client_address))
                         self.request.send("密码错误".encode("utf-8"))
                         del m
                         login_times += 1
             except KeyError:
+                logger("用户[%s]登陆失败：用户名不存在" % auth_data[0], "ERROR", str(self.client_address))
                 self.request.send("用户名不存在".encode("utf-8"))
                 login_times += 1
 
@@ -63,44 +67,45 @@ class TCPHandler(socketserver.BaseRequestHandler):
         temp_path = os.path.join(data_dir, user_pwd, temp_file)  # 临时文件路径
         position = 0
         if used_size + file_size > user_quota:
+            logger("ERROR: 402 - 可用空间不足", "ERROR", str(self.client_address))
             self.request.send("ERROR: 402 - 可用空间不足".encode("utf-8"))
             return
         if os.path.isfile(filepath):
+            logger("ERROR: 409 - 资源已存在：[%s]" % filename, "ERROR", str(self.client_address))
             self.request.send("ERROR: 409 - 资源已存在".encode("utf-8"))
             return
         if os.path.isfile(temp_path):
             position = os.stat(temp_path).st_size
         received_size = position  # 已接收部分
         self.request.send(("200" + " " + str(received_size)).encode("utf-8"))  # 返回请求状态和已接收大小
-        try:
-            m = hashlib.md5()
-            with open(temp_path, "ab") as f:  # 如果文件存在则追加，否则新建
-                while received_size < file_size:
-                    if file_size - received_size > 1024:
-                        buffer_size = 1024
-                    else:
-                        buffer_size = file_size - received_size
-                    # 开始接收数据
-                    _data = self.request.recv(buffer_size)
-                    received_size += len(_data)
-                    f.write(_data)
-                    if not position:
-                        m.update(_data)
-                f.flush()
-            md5sum_server = self.md5sum(temp_path) if position else m.hexdigest()  # 计算md5，如果是断点续传则需要等接收完后重新计算
-            md5sum_client = self.request.recv(1024).decode("utf-8")  # 接收客户端源文件md5值
-            if md5sum_server == md5sum_client:
-                os.rename(temp_path, filepath)
-                config["users"][username]["used"] += file_size  # 计算已使用空间
-                self.save()  # 保存用户状态信息
-                print("文件[%s]接收成功，大小：[%s]，MD5：[%s]" % (filename, file_size, md5sum_server))
-            else:
-                os.remove(temp_path)  # md5校验失败，删除文件
-                print("文件[%s]校验失败，源MD5：[%s] 本地MD5：[%s]" % (filename, md5sum_client, md5sum_server))
-        except PermissionError:
-            self.request.send("ERROR: 403 - 拒绝访问".encode("utf-8"))
-        except (ConnectionAbortedError, ConnectionResetError):
-            print("连接中断...")
+        m = hashlib.md5()
+        with open(temp_path, "ab") as f:  # 如果文件存在则追加，否则新建
+            while received_size < file_size:
+                if file_size - received_size > 1024:
+                    buffer_size = 1024
+                else:
+                    buffer_size = file_size - received_size
+                # 开始接收数据
+                _data = self.request.recv(buffer_size)
+                received_size += len(_data)
+                f.write(_data)
+                if not position:
+                    m.update(_data)
+            f.flush()
+        md5sum_server = self.md5sum(temp_path) if position else m.hexdigest()  # 计算md5，如果是断点续传则需要等接收完后重新计算
+        md5sum_client = self.request.recv(1024).decode("utf-8")  # 接收客户端源文件md5值
+        if md5sum_server == md5sum_client:
+            os.rename(temp_path, filepath)
+            config["users"][username]["used"] += file_size  # 计算已使用空间
+            self.save()  # 保存用户状态信息
+            info = "文件[%s]接收成功，大小：[%s]，MD5：[%s]" % (filename, file_size, md5sum_server)
+            logger(info, "INFO", str(self.client_address))
+            print(info)
+        else:
+            os.remove(temp_path)  # md5校验失败，删除文件
+            info = "文件[%s]校验失败，源MD5：[%s] 本地MD5：[%s]" % (filename, md5sum_client, md5sum_server)
+            logger(info, "ERROR", str(self.client_address))
+            print(info)
 
     def get(self, user_info, params):
         """
@@ -130,10 +135,13 @@ class TCPHandler(socketserver.BaseRequestHandler):
                     f.flush()
                 md5sum_client = self.md5sum(filepath) if sent_size else m.hexdigest()  # 计算md5，如果是断点续传则需要等接收完后重新计算
                 self.request.send(md5sum_client.encode("utf-8"))
+                logger("文件[%s]发送完成" % filename, "INFO", str(self.client_address))
                 print("文件[%s]发送完成" % filename)
             else:
+                logger("ERROR: 402 - 文件为空", "ERROR", str(self.client_address))
                 self.request.send("ERROR: 402 - 文件为空".encode("utf-8"))
         else:
+            logger("ERROR: 404 - 资源不存在：[%s]" % file, "ERROR", str(self.client_address))
             self.request.send("ERROR: 404 - 资源不存在".encode("utf-8"))
 
     def ls(self, user_info, *args):
@@ -159,6 +167,7 @@ class TCPHandler(socketserver.BaseRequestHandler):
             if d == "..":
                 user_pwd = os.path.dirname(user_pwd)  # 获取上一级目录
                 if not user_pwd:  # 已经没有上层了
+                    logger("ERROR: 403 - 拒绝访问：[%s]" % ls_path, "ERROR", str(self.client_address))
                     self.request.send("ERROR: 403 - 拒绝访问".encode("utf-8"))
                     return
             else:
@@ -178,6 +187,7 @@ class TCPHandler(socketserver.BaseRequestHandler):
             else:
                 self.request.send("目录为空".encode("utf-8"))
         else:
+            logger("ERROR: 404 - 无效的文件或目录：[%s]" % ls_path, "ERROR", str(self.client_address))
             self.request.send("ERROR: 404 - 无效的文件或目录".encode("utf-8"))
 
     def cd(self, user_info, params):
@@ -187,29 +197,31 @@ class TCPHandler(socketserver.BaseRequestHandler):
         :param params: 命令参数
         :return: None
         """
+        sep = os.sep
         data_dir = config["server"]["data_dir"]
-        user_pwd = user_info["pwd"]  # 获取当前工作目录
+        user_pwd = user_info["pwd"].rstrip(sep)  # 获取当前工作目录
         username = user_info["name"]
         cd_path = params[0]
-        sep = os.sep
         cd_path = cd_path.replace("/", sep).replace("\\", sep)
         dirs = cd_path.split(sep)  # 拆分多级目录
         if cd_path.startswith(sep):  # 用户目录绝对绝对路径
-            user_pwd = user_info["home"]
+            user_pwd = user_info["home"].rstrip(sep)
         for d in dirs:
             if d == "..":
                 user_pwd = os.path.dirname(user_pwd)  # 获取上一级目录
                 if not user_pwd:  # 已经没有上层了
-                    self.request.send("ERROR: 403 - 拒绝访问".encode("utf-8"))
+                    logger("ERROR: 403 - 拒绝访问非用户目录", "ERROR", str(self.client_address))
+                    self.request.send("ERROR: 403 - 拒绝访问非用户目录".encode("utf-8"))
                     return
             else:
-                user_pwd = os.path.join(user_pwd, d)
+                user_pwd = os.path.join(user_pwd, d).rstrip(sep)
         path = os.path.join(data_dir, user_pwd)
         if os.path.isdir(path):
             config["users"][username]["pwd"] = user_pwd
             self.request.send(user_pwd.encode("utf-8"))
             self.save()
         else:
+            logger("ERROR: 404 - 无效的目录：[%s]" % cd_path, "ERROR", str(self.client_address))
             self.request.send("ERROR: 404 - 无效的目录".encode("utf-8"))
 
     def mkdir(self, user_info, params):
@@ -225,9 +237,11 @@ class TCPHandler(socketserver.BaseRequestHandler):
         mk_path = params[0]
         path = os.path.join(data_dir, user_pwd, mk_path)
         if os.path.isdir(path):
+            logger("ERROR: 409 - 目录已存在：[%s]" % mk_path, "ERROR", str(self.client_address))
             self.request.send("ERROR: 409 - 目录已存在".encode("utf-8"))
         else:
             os.makedirs(path)
+            logger("目录创建成功：[%s]" % mk_path, "INFO", str(self.client_address))
             self.request.send("Success".encode())
 
     def rm(self, user_info, params):
@@ -242,30 +256,31 @@ class TCPHandler(socketserver.BaseRequestHandler):
         user_pwd = config["users"][username]["pwd"]  # 获取当前工作目录
         rm_path = params[0]
         path = os.path.join(data_dir, user_pwd, rm_path)
-        try:
-            if os.path.isfile(path):
-                size = os.stat(path).st_size
-                os.remove(path)
-                config["users"][username]["used"] -= size  # 释放空间，重新计算
+        if os.path.isfile(path):
+            size = os.stat(path).st_size
+            os.remove(path)
+            config["users"][username]["used"] -= size  # 释放空间，重新计算
+            self.save()
+            logger("文件删除成功：[%s]" % rm_path, "INFO", str(self.client_address))
+            self.request.send("Success".encode())
+        elif os.path.isdir(path):
+            size = self.getsize(path)
+            shutil.rmtree(path)
+            if config["users"][username]["used"] - size < 0:
+                config["users"][username]["used"] = 0
                 self.save()
-                self.request.send("Success".encode())
-            elif os.path.isdir(path):
-                size = self.getsize(path)
-                shutil.rmtree(path)
-                if config["users"][username]["used"] - size < 0:
-                    config["users"][username]["used"] = 0
-                    self.save()
-                else:
-                    config["users"][username]["used"] -= size
-                    self.save()
-                self.request.send("Success".encode())
             else:
-                self.request.send("ERROR: 404 - 目标不存在".encode("utf-8"))
-        except PermissionError:
-            self.request.send("ERROR: 403 - 拒绝访问".encode("utf-8"))
+                config["users"][username]["used"] -= size
+                self.save()
+            logger("目录删除成功：[%s]" % rm_path, "INFO", str(self.client_address))
+            self.request.send("Success".encode())
+        else:
+            logger("ERROR: 404 - 目标不存在：[%s]" % rm_path, "ERROR", str(self.client_address))
+            self.request.send("ERROR: 404 - 目标不存在".encode("utf-8"))
 
     def handle(self):
         print("连接已建立 %s" % str(self.client_address))
+        logger("连接已建立", "INFO", str(self.client_address))
         try:
             auth_user = self.auth()  # 用户登陆并获取信息
             while auth_user:
@@ -280,9 +295,11 @@ class TCPHandler(socketserver.BaseRequestHandler):
                     else:
                         func(user_info)  # 无参数命令
                 else:
+                    logger("ERROR: 405 - 无效的指令", "WARN", str(self.client_address))
                     self.request.send("ERROR: 405 - 无效的指令".encode("utf-8"))
-        except (ConnectionResetError, ConnectionAbortedError, IndexError) as e:
-            print("客户端连接中断...")
+        except (ConnectionResetError, ConnectionAbortedError) as e:
+            logger("客户端连接中断：[%s]" % e, "WARN", str(self.client_address))
+            print("客户[%s]端连接中断..." % str(self.client_address))
             return
 
     @staticmethod
@@ -303,8 +320,7 @@ class TCPHandler(socketserver.BaseRequestHandler):
         保存配置信息
         :return: None
         """
-        pwd = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # 获取程序根目录
-        config_path = os.path.join(pwd, "config.yml")  # 组合配置文件路径
+        config_path = os.path.join(root, "config.yml")  # 组合配置文件路径
         if os.path.isfile(config_path):  # 确定配置文件存在并且是文件
             with open(config_path, "w") as f:
                 yaml.dump(config, f)
@@ -328,8 +344,7 @@ def config_parser():
     用于读取yaml格式中的配置文件内容
     :return: 返回读取到的配置内容
     """
-    pwd = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # 获取程序根目录
-    config_path = os.path.join(pwd, "config.yml")  # 组合配置文件路径
+    config_path = os.path.join(root, "config.yml")  # 组合配置文件路径
     if os.path.isfile(config_path):  # 确定配置文件存在并且是文件
         with open(config_path, "r") as f:
             return yaml.load(f, yaml.CLoader if yaml.CLoader else yaml.Loader)  # 读取并返回文件内容
@@ -342,7 +357,6 @@ def initial():
     """
     data_dir = config["server"]["data_dir"]
     if not (data_dir and os.path.isabs(data_dir)):  # 目录未配置或者未配置为绝对路径
-        root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         data_dir = os.path.join(root, data_dir if data_dir else "data")  # 默认数据目录为服务器根目录下的data
         config["server"]["data_dir"] = data_dir  # 将完整路径写入配置
     users = config["users"]
@@ -351,6 +365,22 @@ def initial():
         home_dir = os.path.join(data_dir, home_dir)
         if not os.path.isdir(home_dir):  # 目录不存在时为用户创建家目录
             os.makedirs(home_dir, mode=0o755)
+    logger("服务器初始化完成...", "INFO")
+
+
+def logger(msg, level, client=None):
+    """
+    保存日志
+    :param msg: 要保存的日志消息
+    :param level: 日志级别
+    :param client: 客户端地址
+    :return: None
+    """
+    log_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    log_file = time.strftime("%Y-%m-%d", time.localtime()) + ".log"
+    log_path = os.path.join(root, "logs", log_file)
+    with open(log_path, "a", encoding="utf-8") as f:
+        f.write("[%s] [%s] %s - %s" % (log_time, level, client if client else "-", msg.strip() + "\n"))
 
 
 def main():
@@ -358,7 +388,8 @@ def main():
     主程序
     :return: None
     """
-    global config, platform
+    global config, platform, root
+    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # 获取程序根目录
     config = config_parser()  # 获取配置文件内容
     platform = sys.platform
     initial()
@@ -367,8 +398,10 @@ def main():
         port = int(config["server"]["port"])  # 取出配置中的监听端口
         ftp_server = socketserver.ThreadingTCPServer((ip, port), TCPHandler)  # 实例化socketserver对象
         print("服务器启动，等待连接...")
+        logger("服务器启动，等待连接...", "INFO")
         ftp_server.serve_forever()  # 开始监听
     except (ValueError, KeyError, TypeError) as e:
+        logger("配置文件有误，服务器启动失败：%s" % e, "CRITICAL")
         print("配置文件有误，", e)
         exit(1)
 
