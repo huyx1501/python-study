8304e2b9207b4a316d33f2dc9d9ca4b75e16acd0
huyx1501 <huyx1501@gmail.com>
2019-03-31 19:46:15 +0800

    补充注释

--- Day9/HostManager/core/control.py
+++ Day9/HostManager/core/control.py
@@ -6,12 +6,26 @@ import paramiko
 
 
 class Controller(object):
+    """
+    远程控制类，执行远程命令和文件上传
+    """
     def __init__(self, host, username, password):
+        """
+        构造函数
+        :param host: 主机地址和端口的元组
+        :param username: 用户名
+        :param password: 密码
+        """
         self.host = host
         self.username = username
         self.password = password
 
     def exec(self, cmd):
+        """
+        执行shell命令
+        :param cmd: 要执行的命令
+        :return: None
+        """
         shell = paramiko.SSHClient()
         shell.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         shell.connect(hostname=self.host[0], port=self.host[1], username=self.username, password=self.password)
@@ -29,6 +43,12 @@ class Controller(object):
         shell.close()
 
     def put(self, src, dest):
+        """
+        上传文件到远程服务器
+        :param src: 源文件路径
+        :param dest: 目标文件路径
+        :return: None
+        """
         transport = paramiko.Transport(self.host)
         transport.connect(username=self.username, password=self.password)
         scp = paramiko.SFTPClient.from_transport(transport)
--- Day9/HostManager/core/manager.py
+++ Day9/HostManager/core/manager.py
@@ -12,23 +12,33 @@ config_file = os.path.join(root_dir, "conf", "config.yml")
 
 
 def config_parser():
+    """
+    读取配置文件
+    :return: 返回配置文件内容
+    """
     with open(config_file, "r", encoding="utf-8") as f:
         conf = yaml.load(f, yaml.CLoader if yaml.CLoader else yaml.Loader)
     return conf
 
 
 def shell(cmd, hosts):
+    """
+    解析命令并执行
+    :param cmd: 要执行的命令
+    :param hosts: 执行命令的目标主机
+    :return: None
+    """
     thread_list = []
     if cmd.startswith("put"):  # 上传文件
         cmd = cmd.split()
         if len(cmd) < 3:
             print("参数错误")
             return
-        src = cmd[1]
-        dest = cmd[2]
+        src = cmd[1]  # 源文件
+        dest = cmd[2]  # 目标文件
         dest_filename = os.path.basename(dest) if os.path.basename(dest) else os.path.basename(src)
-        dest_path = os.path.dirname(dest)+ "/" + dest_filename
-        if not os.path.isabs(src):
+        dest_path = os.path.dirname(dest) + "/" + dest_filename
+        if not os.path.isabs(src):  # 相对路径
             src_path = os.path.join(os.path.dirname(__file__), src)
         else:
             src_path = src
@@ -36,7 +46,7 @@ def shell(cmd, hosts):
             for host in hosts:
                 controller = control.Controller((host["ipaddr"], int(host["port"])), host["username"],
                                                 str(host["password"]))
-                thread = threading.Thread(target=controller.put(src_path, dest_path))
+                thread = threading.Thread(target=controller.put, args=(src_path, dest_path))  # 使用新线程执行上传指令
                 thread.start()
                 thread_list.append(thread)
         else:
@@ -55,15 +65,19 @@ def shell(cmd, hosts):
 
 
 def main():
+    """
+    主程序，与用户交互
+    :return: None
+    """
     global config
-    config = config_parser()
-    groups = config["groups"]
+    config = config_parser()  # 获取配置文件内容
+    groups = config["groups"]  # 获取主机组信息
     for i, group in enumerate(groups):
         print("%d) - %s 主机数：%d" % (i+1, group["group_name"], len(groups[i]["hosts"])))
     try:
         choice = int(input("请选择主机组："))
         select_group = groups[choice-1]
-        hosts = select_group["hosts"]
+        hosts = select_group["hosts"]  # 获取指定组下面所有主机
         for i, host in enumerate(hosts):
             print("\t%d) - %s: %s" % (i + 1, host["hostname"], host["ipaddr"]))
         choice = input("请选择主机编号，直接回车将进入批量模式：")
@@ -78,12 +92,11 @@ def main():
             if not cmd:
                 continue
             elif cmd == "help":
-                print('''使用帮助：
-            1. 直接输入命令在远程主机执行命令
-            2. 输入put命令上传文件到远程目录
-            3. 输入bye退出
-            4. 输入help显示帮助
-            ''')
+                print('''===使用帮助===：
+1. 直接输入命令在远程主机执行命令
+2. 输入put命令上传文件到远程目录
+3. 输入bye退出
+4. 输入help显示帮助''')
                 continue
             elif cmd == "bye":
                 print("Bye")
