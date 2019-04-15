e47ca719e24df2edeef06bb179494f22951710b6
huyx1501 <huyx1501@gmail.com>
2019-04-15 18:40:13 +0800

    高并发FTP-初始化

---
+++ Day10/MyFTP_Paralleled/server/startup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python36
+# -*- coding: utf-8 -*-
+# Author: Bob
+
+import core.server
+
+
+def main():
+    print("欢迎使用MyFTP")
+    core.server.main()
+
+
+if __name__ == "__main__":
+    main()
