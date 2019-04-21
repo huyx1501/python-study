3061f75c3b7bce932e591dc00f88b4f9a5bf17f9
huyx1501 <huyx1501@gmail.com>
2019-04-21 21:36:55 +0800

    direct模式的消息推送与订阅

---
+++ Day11/rabbitmq_subscriber_direct.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python36
+# -*- coding: utf-8 -*-
+# Author: Bob
+
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="192.168.80.10"))
+channel = connection.channel()
+channel.exchange_declare(exchange="logs", exchange_type="direct")  # 声明一个exchange，类型为direct
+result = channel.queue_declare(queue="", exclusive=True)  # 通过指定空白的队列名称来生成一个随机的queue,exclusive声明为唯一
+queue_name = result.method.queue  # 获取队列名称
+print("Queue name: ", queue_name)
+levels = input("Log levels for subscriber: >>").strip().split()
+# 根据输入的监听级别，循环绑定routing_key到exchange中
+for level in levels:
+    channel.queue_bind(queue=queue_name, exchange="logs", routing_key=level)
+
+
+def callback(ch, method, properties, body):
+    print("Received message:")
+    print("""ch: %s
+method: %s
+properties: %s
+body: %s""" % (ch, method, properties, body))
+    ch.basic_ack(delivery_tag=method.delivery_tag)  # 如果没有自动确认则需手工调用确认方法,method.delivery_tag为消息编号
+
+
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=False)
+print("Waiting for message.")
+channel.start_consuming()
+connection.close()
